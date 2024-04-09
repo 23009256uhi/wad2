534933542136738ba65503fb38c42b5d77a65eb7
23009256uhi <23009256uhi@gmail.com>
2024-04-09 22:27:15 +0100

    docker-compose + dockerfile

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,13 +1,28 @@
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PASSWORD: "postgres",
+//     DB: "testdb",
+//     dialect: "postgres",
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     }
+//   };
+
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "postgres",
-    DB: "testdb",
-    dialect: "postgres",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+  HOST: process.env.DB_HOST || "db",
+  PORT: process.env.DB_PORT || "5432",
+  USER: process.env.DB_USER || "postgres",
+  PASSWORD: process.env.DB_PASSWORD || "postgres",
+  DB: process.env.DB_NAME || "testdb",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
